ff2c74406bb4bc0fa0b48c56ef363968eff4a088
Andrew <atandiawan@gmail.com>
2016-09-14 18:03:30 +0700

    Release

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 "use strict"
 
-import DBModel from "./models/db_model.js";
-import Cohort from "./models/cohort.js";
-import Student from "./models/student.js";
+//import DBModel from "./models/db_model.js";
+import Cohorts from "./models/cohort.js";
+import Students from "./models/student.js";
+
+let repl = require('repl')
+let sqlite = require('sqlite3').verbose()
+let fs = require('fs')
+let file = 'db/contacts.db'
+let db = new sqlite.Database(file)
+
+let replServer = repl.start(">")
+replServer.context.Cohorts = Cohorts
+replServer.context.Students = Students
+replServer.context.db = db
+
+// Cohorts.save(db, new Cohorts("Physics"))
+// Cohorts.save(db, new Cohorts("Chemistry"))
+// Cohorts.save(db, new Cohorts("Biology"))
+// Cohorts.save(db, new Cohorts("Math"))
+Students.save(db, new Students("Soni","Setiawan","3"))
+Students.save(db, new Students("Malik","Bum","1"))
+Students.readTable(db)
