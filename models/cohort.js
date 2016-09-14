cda1ed2e4f8c138a26847787afa31de59d521383
Ahyana Rizky Pratama & Ryan Andhaka <pair+ahyanarizky.ryanandhaka@hacktiv8.com>
2016-09-14 18:53:08 +0700

    orm1

--- models/cohort.js
+++ models/cohort.js
@@ -3,7 +3,53 @@
 import Student from "./student.js";
 
 class Cohort {
+  constructor(property){
+    this.name = property
+  }
 
+  set name(value){this.name = value}
+  get name(){return this.name}
+
+  static create(db, cohort) {
+    if (student instanceof Cohort) {
+      let stmt = "insert into cohorts (name) values(?);";
+      db.run(stmt, [cohort.name], (err) => {
+        if (err) console.log(err);
+      })
+    }
+  }
+  static find(db, target, value) {
+      let query = `select * from cohort where ${target} = ${value};`
+      db.run(query, function(err, result) {
+        if (err){
+          console.log(err);
+        } else {
+          console.log('Data found :\n');
+          console.log(result);
+        }
+      })
+  }
+  static delete(db, target, value) {
+    let query = `delete from cohort where ${target} = ${value};`
+    db.run(query, function(err, result) {
+      if (err){
+        console.log(err);
+      } else {
+        console.log(`${value} was deleted from table;`);
+      }
+    })
+  }
+
+  static edit(db, target, value) {
+    let query = `update cohort set ${target} where ${value};`
+    db.run(query, function(err, result) {
+      if (err){
+        console.log(err);
+      } else {
+        console.log(`${target} was updated`);
+      }
+    })
+  }
 
 }
 
--- models/db_model.js
+++ models/db_model.js
@@ -1,8 +1,47 @@
 "use strict"
 
+// write your code here
+var CREATE_TABLE = "CREATE TABLE IF NOT EXISTS students ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, grade TEXT, email TEXT, score INTEGER);"
+
+// CREATE TABLE IF NOT EXISTS cohort ( id INTEGER PRIMARY KEY AUTOINCREMENT, cohort_name TEXT );"
+var CREATE_TABLE_COHORT = "CREATE TABLE IF NOT EXISTS cohorts ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);";
+
+
 class DBModel {
+  constructor(filename, sqlite){
+    this.connection = new sqlite.Database(filename)
+    DBModel.init(this.connection)
+  }
 
-
+  get connection(){
+    return this.connection
+  }
+  static init(connection){
+    connection.serialize(function () {
+      connection.run(CREATE_TABLE,function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`table student created`);
+          }
+      });
+    });
+    connection.serialize(function () {
+      connection.run(CREATE_TABLE_COHORT,function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`table cohort created`);
+          }
+      });
+    });
+  }
 }
+
+
+
+
+//CREATE TABLE
+
 
 export default DBModel
